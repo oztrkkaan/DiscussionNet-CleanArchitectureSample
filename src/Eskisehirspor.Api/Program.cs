2338ea233daf20c7520d55ac0c60d97eb763bafa
Kaan Öztürk <oztrkkaan@gmail.com>
2022-09-01 23:17:56 +0300

    TokenService implementasyonu

--- src/Eskisehirspor.Api/Program.cs
+++ src/Eskisehirspor.Api/Program.cs
@@ -1,5 +1,10 @@
 using Eskisehirspor.Application;
+using Eskisehirspor.Infrastructure;
+using Eskisehirspor.Infrastructure.Authentication.Jwt;
 using Eskisehirspor.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +15,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureLayer();
 builder.Services.AddPersistenceLayer(builder.Configuration);
-
+AddJwtService(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,3 +34,23 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddJwtService(IServiceCollection services, IConfiguration configuration)
+{
+    var tokenOptions = configuration.GetSection("TokenOptions").Get<TokenOptions>();
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(options =>
+      {
+          options.TokenValidationParameters = new TokenValidationParameters
+          {
+              ValidateAudience = true,
+              ValidateIssuer = true,
+              ValidateLifetime = true,
+              ValidateIssuerSigningKey = true,
+              ValidAudience = tokenOptions.Audience,
+              ValidIssuer = tokenOptions.Issuer,
+              IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey)),
+              LifetimeValidator = (notBefore, expires, tokenToValidate, tokenValidationParameters) => expires != null && expires > DateTime.UtcNow
+          };
+      });
+}
