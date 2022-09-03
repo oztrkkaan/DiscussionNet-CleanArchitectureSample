894c73c90aa84027b58731e6d1868366ada9ffef
Kaan Öztürk <oztrkkaan@gmail.com>
2022-09-03 20:10:34 +0300

    Topic and Thread EF configurations.

---
+++ src/Eskisehirspor.Persistence/Configurations/TopicConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Eskisehirspor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eskisehirspor.Persistence.Configurations
+{
+    public class TopicConfiguration : IEntityTypeConfiguration<Topic>
+    {
+        public void Configure(EntityTypeBuilder<Topic> builder)
+        {
+            AuditableEntityConfiguration<Topic>.SetProperties(builder);
+            SoftDeleteConfiguration<Topic>.SetProperties(builder);
+
+            builder.Property(p => p.Subject)
+                 .IsRequired()
+                 .HasMaxLength(Topic.SUBJECT_MAX_LENGTH);
+
+            builder.Ignore(p => p.UrlName);
+            builder.Ignore(p => p.Tags);
+        }
+    }
+}
