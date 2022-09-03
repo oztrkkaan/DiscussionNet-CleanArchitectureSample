d4f4e25a366cc95119ad895a00c698471b5b82a8
Kaan Öztürk <oztrkkaan@gmail.com>
2022-09-03 20:09:37 +0300

    SendRegistrationEmailEvent added.

---
+++ src/Eskisehirspor.Application/UseCases/Email/RegistrationEmail/Publisher/SendRegistrationEmailEvent.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MediatR;
+
+namespace Eskisehirspor.Application.UseCases.Email.RegistrationEmail.Publisher
+{
+    public class SendRegistrationEmailEvent : INotification
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class SendRegistrationEmailEventHandler : INotificationHandler<SendRegistrationEmailEvent>
+    {
+        ISendEndpointProvider _sendEndpointProvider;
+        private const string EMAILSERVICE_QUEUE_NAME = "emailservice.registration";
+        public SendRegistrationEmailEventHandler(ISendEndpointProvider sendEndpointProvider)
+        {
+            _sendEndpointProvider = sendEndpointProvider;
+        }
+
+        public async Task Handle(SendRegistrationEmailEvent notification, CancellationToken cancellationToken)
+        {
+            var endpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{EMAILSERVICE_QUEUE_NAME}"));
+            await endpoint.Send(notification, cancellationToken);
+        }
+    }
+}
