61fbef8ba47aa2dee5cb2cdc35dbbdc2da0a3385
Kaan Öztürk <oztrkkaan@gmail.com>
2022-09-08 20:31:52 +0300

    json extension eklendi

---
+++ src/Eskisehirspor.Application/Common/Extensions/JsonExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Text.Json;
+
+namespace Eskisehirspor.Application.Common.Extensions
+{
+    public static class JsonExtenstions
+    {
+        public static string ToJSON(this object serializedObject)
+        {
+            JsonSerializerOptions serializerSettings = new JsonSerializerOptions();
+            //serializerSettings.Converters.Add(new StringConverter());
+            return JsonSerializer.Serialize(serializedObject, serializerSettings);
+        }
+
+        public static string ToJSON(this object serializedObject, JsonSerializerOptions serializerSettings)
+        {
+            return JsonSerializer.Serialize(serializedObject, serializerSettings);
+        }
+
+        public static T DeserializeJSON<T>(this string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public static object DeserializeJSON(this string json, Type type)
+        {
+            return JsonSerializer.Deserialize(json, type);
+        }
+
+    }
+}
