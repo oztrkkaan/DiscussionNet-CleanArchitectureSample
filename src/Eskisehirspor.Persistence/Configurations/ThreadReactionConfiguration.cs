f28587d925e7eb28f50d6b978b8c23875826d2bc
Kaan Öztürk <oztrkkaan@gmail.com>
2022-09-08 20:36:16 +0300

    ThreadReactionConfiguration added

---
+++ src/Eskisehirspor.Persistence/Configurations/ThreadReactionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Eskisehirspor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eskisehirspor.Persistence.Configurations
+{
+    public class ThreadReactionConfiguration : IEntityTypeConfiguration<ThreadReaction>
+    {
+        public void Configure(EntityTypeBuilder<ThreadReaction> builder)
+        {
+            AuditableEntityConfiguration<ThreadReaction>.SetProperties(builder);
+            SoftDeleteConfiguration<ThreadReaction>.SetProperties(builder);
+
+            builder.Property(p => p.Reaction)
+                .IsRequired()
+                .HasConversion<byte>();
+
+            builder.HasOne(m => m.Thread).WithMany(m => m.Reactions);
+            builder.HasOne(m => m.ReactedBy).WithMany(m => m.Reactions);
+        }
+    }
+}
