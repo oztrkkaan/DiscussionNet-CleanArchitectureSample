9935926234e288de33aeb3178f98265452c2664f
Kaan Öztürk <oztrkkaan@gmail.com>
2022-09-08 20:43:14 +0300

    Email publisher consumer added

--- src/Eskisehirspor.Application/UseCases/Email/RegistrationEmail/Publisher/SendRegistrationEmailPublisher.cs
+++ src/Eskisehirspor.Application/UseCases/Email/RegistrationEmail/Publisher/SendRegistrationEmailPublisher.cs
@@ -3,23 +3,23 @@ using MediatR;
 
 namespace Eskisehirspor.Application.UseCases.Email.RegistrationEmail.Publisher
 {
-    public class SendRegistrationEmailEvent : INotification
+    public class SendRegistrationEmailPublisher : INotification
     {
         public string Email { get; set; }
         public string Username { get; set; }
         public string DisplayName { get; set; }
     }
 
-    public class SendRegistrationEmailEventHandler : INotificationHandler<SendRegistrationEmailEvent>
+    public class SendRegistrationEmailPublisherHandler : INotificationHandler<SendRegistrationEmailPublisher>
     {
         ISendEndpointProvider _sendEndpointProvider;
         private const string EMAILSERVICE_QUEUE_NAME = "emailservice.registration";
-        public SendRegistrationEmailEventHandler(ISendEndpointProvider sendEndpointProvider)
+        public SendRegistrationEmailPublisherHandler(ISendEndpointProvider sendEndpointProvider)
         {
             _sendEndpointProvider = sendEndpointProvider;
         }
 
-        public async Task Handle(SendRegistrationEmailEvent notification, CancellationToken cancellationToken)
+        public async Task Handle(SendRegistrationEmailPublisher notification, CancellationToken cancellationToken)
         {
             var endpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{EMAILSERVICE_QUEUE_NAME}"));
             await endpoint.Send(notification, cancellationToken);
---
+++ src/Eskisehirspor.Application/UseCases/Email/RegistrationEmail/SendRegistrationEmailEvent.cs
@@ -0,0 +1,38 @@
+﻿using Eskisehirspor.Application.Common.Interfaces;
+using MediatR;
+
+namespace Eskisehirspor.Application.UseCases.Email.RegistrationEmail
+{
+    public class SendRegistrationEmailEvent : INotification
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string ActivationUrl { get; set; }
+    }
+
+    public class SendRegistrationEmailEventHandler : INotificationHandler<SendRegistrationEmailEvent>
+    {
+        private readonly IMailService _mailService;
+        private const string EMAIL_SUBJECT = "E-Posta Doğrulama - eskisehirspor.com";
+
+        public SendRegistrationEmailEventHandler(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public async Task Handle(SendRegistrationEmailEvent notification, CancellationToken cancellationToken)
+        {
+            var emailBody =
+            $@"Aramıza hoşgeldin {notification.DisplayName}, 
+            <br/><br/>
+            <a href='https://eskisehirspor.com'>eskisehirspor.com</a> hesabınıza {notification.Email} e-posta adresinizi kullanarak veya <b>{notification.Username}</b> kullanıcı adıyla giriş yapabilirsiniz.
+            <br/><br/>
+            E-posta adresinizi doğrulamak için aşağıdaki bağlantıyı tıkla:
+            <a href='{notification.ActivationUrl}'>{notification.ActivationUrl}</a>
+            ";
+
+            await _mailService.SendMailAsync(EMAIL_SUBJECT, emailBody, new List<string> { notification.Email }, Common.Mailing.MailName.Bilgi);
+        }
+    }
+}
